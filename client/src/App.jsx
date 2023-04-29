b761940423c3361a5fd701f41daf3950af447630
Amer Shboul <71199669+shboul1@users.noreply.github.com>
2023-04-29 13:16:56 +0300

    prepare for deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-const BASE_URL = "https://localhost:8000/cards";
+const BASE_URL = "https://54.197.134.142:8000/cards";
 
 function App() {
   const [cards, setCards] = useState([]);
